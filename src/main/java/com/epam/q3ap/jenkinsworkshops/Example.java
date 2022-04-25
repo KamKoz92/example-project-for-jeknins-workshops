6d220874562865a3799e15c65630bee9127a7a54
Kamil Koziol <kamil_koziol@epam.com>
2022-04-25 12:05:28 +0200

    commit

--- src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
+++ src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
@@ -10,5 +10,6 @@ package com.epam.q3ap.jenkinsworkshops;
 class Example {
   public static void main(String[] args) {
     System.out.println("Hello world");
+    System.out.println("Goodbye world");
   }
 }
